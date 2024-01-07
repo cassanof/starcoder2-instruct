4f3b1a88f7285647df11bf9be022e7dc820ba610
cassanof <federico.cassano@federico.codes>
2024-01-07 14:10:57 -0500

    ds

--- ds.py
+++ ds.py
@@ -11,9 +11,10 @@ def add_markdown_code_block(s: str, tag="python") -> str:
 
 # <ds getter> -> (<context block>, <instruction>, <code block>)
 DATASETS: Dict[DatasetGetter, Tuple[ExampleTransform, ExampleTransform, ExampleTransform]] = {
-    (lambda: datasets.load_dataset("nuprl/EditPackFT", split="train[:100000]")): (  # type: ignore
+    (lambda: datasets.load_dataset("nuprl/EditPackFT-Multi", split="train[:100000]")): (  # type: ignore
         lambda x: add_markdown_code_block(x["old_contents"]),
         lambda x: x["instruction"],
-        lambda x: x["new_contents"],
+        lambda x: add_markdown_code_block(x["new_contents"]),
     ),
+    # ise-uiuc/Magicoder-OSS-Instruct-75K
 }
